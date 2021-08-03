5a5545a554768c7dee1411c8d67eed3a996d0e1b
Luka Racic <lracic2406@gmail.com>
2021-08-03 23:18:59 +0200

    trying to use vs code to push commit

--- website/__init__.py
+++ website/__init__.py
@@ -40,3 +40,6 @@ def create_database(app):
     if not path.exists("website/"+DB_NAME):
         db.create_all(app=app)
         print("Database created!")
+
+
+#added some comment, so I can try git
