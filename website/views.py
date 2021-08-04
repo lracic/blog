d9b600414465b4fbc6761b8fa85dea94a4c30988
Luka Racic <lracic2406@gmail.com>
2021-08-04 10:24:10 +0200

    end of tutorial #3

--- website/views.py
+++ website/views.py
@@ -1,9 +1,11 @@
 import flask_login
+from flask_sqlalchemy import utils
+from sqlalchemy.sql.functions import user
 from website import create_database
 from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
 from flask.helpers import flash
-from .models import Post
+from .models import Post, User
 from . import db
 
 views = Blueprint("views", __name__)
@@ -34,3 +36,32 @@ def create_post():
             return redirect(url_for('views.home'))
 
     return render_template('create_post.html', user=current_user)
+
+
+@views.route("/delete-post/<id>")
+@login_required
+def delete_post(id):
+    post = Post.query.filter_by(id=id).first()
+
+    if not post:
+        flash("Post does not exist!", category='error')
+    elif current_user.id != post.id:
+        flash("You do not have permission to delete the post!", category='error')
+    else:
+        db.session.delete(post)
+        db.session.commit()
+        flash("Post deleted!", category='success')
+
+    return redirect(url_for('views.home'))
+
+
+@views.route("/posts/<username>")
+@login_required
+def posts(username):
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        flash("No user with that username exists!", category='error')
+        return redirect(url_for('views.home'))
+    posts = Post.query.filter_by(author=user.id).all()
+
+    return render_template("posts.html", user=current_user, posts=posts, username=username)
