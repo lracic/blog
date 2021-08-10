03f771155375067f249d98a902eb53d45e84aa7f
Luka Racic <lracic2406@gmail.com>
2021-08-10 20:19:25 +0200

    end of tutorial 5

--- website/views.py
+++ website/views.py
@@ -1,3 +1,4 @@
+import re
 import flask_login
 from flask_sqlalchemy import utils
 from sqlalchemy.sql.functions import user
@@ -5,7 +6,7 @@ from website import create_database
 from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
 from flask.helpers import flash
-from .models import Post, User, Comment
+from .models import Like, Post, User, Comment
 from . import db
 
 views = Blueprint("views", __name__)
@@ -100,4 +101,22 @@ def delete_comment(comment_id):
         db.session.delete(comment)
         db.session.commit()
 
+    return redirect(url_for('views.home'))
+
+
+@views.route("/like-post/<post_id>", methods=['GET'])
+@login_required
+def like(post_id):
+    post = Post.query.filter_by(id=post_id)
+    like = Like.query.filter_by(
+        author=current_user.id, post_id=post_id).first()
+    if not post:
+        flash('Post does not exist', category='error')
+    elif like:
+        db.session.delete(like)
+        db.session.commit()
+    else:
+        like = Like(author=current_user.id, post_id=post_id)
+        db.session.add(like)
+        db.session.commit()
     return redirect(url_for('views.home'))
